d40144bdb48d6d18ea3dee841ce62294c2ff70c6
Daniil <dadyarri@gmail.com>
2023-05-14 02:57:30 +0300

    add icon to button

--- src/assets/js/switchTheme.js
+++ src/assets/js/switchTheme.js
@@ -2,23 +2,18 @@ function switchTheme () {
     const v = localStorage.getItem('darkmode') === "true";
     const darkmode = !v;
     localStorage.setItem('darkmode', darkmode);
-    if (darkmode)
-    {
-        document.body.classList.add("dark");
-    }
-    else
-    {
-        document.body.classList.remove("dark");
-    }
+    refreshTheme();
 }
 
 function refreshTheme () {
     if (localStorage.getItem('darkmode') === 'true') {
         document.body.classList.add("dark");
+        document.getElementById("themeSwitcherIcon").src = "assets/img/sun.svg";
     }
     else
     {
         document.body.classList.remove("dark");
+        document.getElementById("themeSwitcherIcon").src = "assets/img/moon.svg";
     }
 }
 
@@ -28,4 +23,4 @@ window.onload = function () {
 
 document.addEventListener("turbolinks:load", function() {
     refreshTheme();
-  })
+})
