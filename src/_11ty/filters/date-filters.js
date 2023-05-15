3ecb915b9f936583859b2c98f8171af699e8b27e
Daniil <dadyarri@gmail.com>
2023-05-16 00:39:32 +0300

    force russian date format

--- src/_11ty/filters/date-filters.js
+++ src/_11ty/filters/date-filters.js
@@ -2,7 +2,7 @@ const { DateTime } = require('luxon');
 
 // Date formatting (human readable)
 function readableDateFilter (dateObj) {
-  return DateTime.fromJSDate(dateObj).toFormat('DDD');
+  return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL, {locale: "ru"});
 };
 
 // Date formatting (machine readable)
