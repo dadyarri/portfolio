6b64537ceecde837922021430ec553eb89cb3557
Daniil <dadyarri@gmail.com>
2023-05-16 00:22:31 +0300

    configure eleventy to generate og images for posts

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,8 @@ const browserSyncConfig = require('./src/_11ty/utils/browser-sync-config');
 const tableOfContents = require('eleventy-plugin-toc');
 const { readableDateFilter, machineDateFilter } = require('./src/_11ty/filters/date-filters');
 const cardShortcode = require('./src/_11ty/shortcodes/card-shortcode');
+const fs = require("fs");
+const Image = require('@11ty/eleventy-img');
 
 module.exports = function (eleventyConfig) {
   // Plugins
@@ -24,6 +26,27 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('readableDate', readableDateFilter);
   eleventyConfig.addFilter('machineDate', machineDateFilter);
   eleventyConfig.addFilter('svg', svgFilter);
+  eleventyConfig.addFilter('splitlines', function (input) {
+    const parts = input.split(' ');
+    const lines = parts.reduce(function (prev, current) {
+
+      if (!prev.length) {
+        return [current];
+      }
+
+      let lastOne = prev[prev.length - 1];
+
+      if (lastOne.length + current.length > 19) {
+        return [...prev, current];
+      }
+
+      prev[prev.length - 1] = lastOne + ' ' + current;
+
+      return prev;
+    }, []);
+
+    return lines;
+  });
 
   // Shortcodes
   eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
@@ -55,6 +78,32 @@ module.exports = function (eleventyConfig) {
 
   // Allow Turbolinks to work in development mode
   eleventyConfig.setBrowserSyncConfig(browserSyncConfig);
+
+  eleventyConfig.on('afterBuild', () => {
+    const socialPreviewImagesDir = "_site/img/og/";
+    fs.readdir(socialPreviewImagesDir, function (err, files) {
+        if (files.length > 0) {
+            files.forEach(function (filename) {
+                if (filename.endsWith(".svg")) {
+
+                    let imageUrl = socialPreviewImagesDir + filename;
+                    Image(imageUrl, {
+                        formats: ["jpeg"],
+                        outputDir: "./" + socialPreviewImagesDir,
+                        filenameFormat: function (id, src, width, format, options) {
+
+                            let outputFilename = filename.substring(0, (filename.length-4));
+                        
+                            return `${outputFilename}.${format}`;
+
+                        }
+                    });
+
+                }
+            })
+        }
+    })
+  });
 
   return {
     templateFormats: ['md', 'njk', 'html'],
