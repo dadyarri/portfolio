274689b2978a56bfb9489378d0157bbfa9c719dc
Daniil <dadyarri@gmail.com>
2023-08-28 13:04:12 +0300

    update list of projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,7 +9,7 @@ export type Project = {
 const projects: Project[] = [
   {
     title: "ChocoManager. Backend",
-    techs: ["C#", "ASP.NET Core", "Docker"],
+    techs: ["C#", "ASP.NET", "Docker"],
     link: "https://github.com/choco-manager/Backend",
     description: "Бэк-офис небольшого интернет-магазина"
   },
@@ -19,6 +19,11 @@ const projects: Project[] = [
     link: "https://github.com/choco-manager/Application",
     description: "Мобильное приложение для бэк-офиса интернет магазина",
     isComingSoon: true
+  },
+  {
+    title: "AudioSplttr",
+    techs: ["C#", "WPF"],
+    description: "Приложение, разделяющее аудиодорожку на фрагменты по уровню тишины"
   }
 ];
 
