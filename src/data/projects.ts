5b77ad7b4e7c71f5c27880e2ec3c14189eb6c208
Daniil <dadyarri@gmail.com>
2023-09-20 23:15:28 +0300

    add updated project cards to home page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,30 +1,7 @@
 export type Project = {
+  slug: string;
   title: string;
-  techs: string[];
-  link?: string;
-  description?: string;
-  isComingSoon?: boolean;
+  description: string;
+  tags: string[];
+  subpage: boolean;
 };
-
-const projects: Project[] = [
-  {
-    title: "ChocoManager. Backend",
-    techs: ["C#", "ASP.NET", "Docker"],
-    link: "https://github.com/choco-manager/Backend",
-    description: "Бэк-офис небольшого интернет-магазина"
-  },
-  {
-    title: "ChocoManager. Application",
-    techs: ["Kotlin", "Jetpack Compose"],
-    link: "https://github.com/choco-manager/Application",
-    description: "Мобильное приложение для бэк-офиса интернет магазина",
-    isComingSoon: true
-  },
-  {
-    title: "AudioSplttr",
-    techs: ["C#", "WPF"],
-    description: "Приложение, разделяющее аудиодорожку на фрагменты по уровню тишины"
-  }
-];
-
-export default projects;
