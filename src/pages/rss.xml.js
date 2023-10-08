feaf7a0070714c959c3ce211b9c3bcd84fc04388
Daniil <dadyarri@gmail.com>
2023-10-08 17:30:19 +0300

    some fixes to rss generator

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,19 +2,25 @@ import rss from '@astrojs/rss';
 
 import { getCollection } from 'astro:content';
 
+import sanitizeHtml from 'sanitize-html';
+
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
 export async function GET(context) {
     const posts = await getCollection('posts');
     return rss({
         title: 'Личный блог dadyarri',
         description: 'Потоки мыслей на рандомные темы и околотехническая болтовня',
         site: context.site,
-        items: posts.map(it => ({
+        items: posts.map((it) => ({
             title: it.data.title,
             pubDate: it.data.publishedAt,
             description: it.data.description,
-            link: `/posts/${it.slug}/`
+            link: `/posts/${it.slug}/`,
+            content: sanitizeHtml(parser.render(it.body)),
         })),
-        customData: `<language>ru-ru</language>`,
+        customData: '<language>ru-ru</language>',
         stylesheet: '/assets/feed-style.xsl',
     });
 }
