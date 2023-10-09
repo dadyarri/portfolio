363eb4aa3100142f906467cf05a5744ee58588ec
Daniil <dadyarri@gmail.com>
2023-10-09 10:01:07 +0300

    update cv styles

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -6,12 +6,12 @@ import { chromium } from 'playwright';
 
   await page.goto('http://localhost:4321/cv', { waitUntil: 'networkidle' });
 
-  const downloadButton = page.locator('a[download]');
+  const downloadButton = page.locator('a#download');
   await downloadButton.evaluate((node) => (node.innerHTML = ''));
 
   const body = page.locator('body');
-  await body.evaluate((node) => node.classList.remove('bg-indigo-50'));
-  await body.evaluate((node) => node.classList.remove('sm:shadow-md'));
+  await body.evaluate((node) => node.classList.remove('sm:my-14'));
+  // await body.evaluate((node) => node.classList.remove(''));
 
   await page.pdf({
     path: 'public/cv.pdf',
