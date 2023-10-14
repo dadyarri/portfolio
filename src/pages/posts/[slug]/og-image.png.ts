04184c9c7ebfbc6b2eb20a14c0211044893f87fc
Daniil <dadyarri@gmail.com>
2023-10-14 15:10:38 +0300

    add script to generate og images on posts

---
+++ src/pages/posts/[slug]/og-image.png.ts
@@ -0,0 +1,93 @@
+import fs from "fs/promises";
+import type { APIRoute } from "astro";
+import { getCollection, getEntryBySlug } from "astro:content";
+import satori from "satori";
+import sharp from "sharp";
+import formatDate from "@/utils/formatDate";
+import getReadingTime from 'reading-time';
+
+export async function getStaticPaths() {
+    const posts = await getCollection("posts");
+    return posts.map((post) => ({
+        params: { slug: post.slug },
+        props: post,
+    }));
+}
+
+export const GET: APIRoute = async ({ params }) => {
+    const page = (await getEntryBySlug("posts", params.slug!))!;
+    const openSansData = await fs.readFile("./public/fonts/open-sans/OpenSans-Regular.ttf");
+    const readingTime = getReadingTime(page.body)
+    const estimateMinutes = Math.ceil(readingTime.minutes);
+
+    const svg = await satori(
+        {
+            type: "div",
+            props: {
+                children: [
+                    {
+                        type: "h1",
+                        props: {
+                            children: page.data.title,
+                            style: {
+                                fontFamily: "Open Sans",
+                                fontSize: "64px",
+                                lineHeight: 1,
+                                color: "#111",
+                                marginBottom: "32px",
+                            },
+                        }
+                    },
+                    {
+                        type: "p",
+                        props: {
+                            children: `${formatDate(page.data.publishedAt)} | dadyarri | Читать ${estimateMinutes} минут`,
+                            style: {
+                                fontFamily: "Open Sans",
+                                fontSize: "20px",
+                                lineHeight: 1,
+                                color: "#444",
+                            },
+                        }
+                    }
+                ],
+                style: {
+                    width: 1200,
+                    height: 630,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    borderBottom: "20px solid rgb(53, 88, 255)",
+                    padding: "80px",
+                    background: "#fff"
+                },
+            }
+        },
+        {
+            width: 1200,
+            height: 630,
+            fonts: [
+                {
+                    name: "Open Sans",
+                    data: openSansData,
+                    weight: 400,
+                    style: "normal",
+                },
+                {
+                    name: "Open Sans",
+                    data: openSansData,
+                    weight: 600,
+                    style: "normal",
+                },
+            ],
+        }
+    );
+
+    const png = await sharp(Buffer.from(svg)).png().toBuffer();
+
+    return new Response(png, {
+        headers: {
+            "Content-Type": "image/png",
+        },
+    });
+}
