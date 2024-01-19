06ac6357e7ade2d455a573463caa1c8219fb5325
Daniil <dadyarri@gmail.com>
2024-01-19 21:23:28 +0300

    add info for index page

---
+++ src/data/presentation.ts
@@ -0,0 +1,43 @@
+type Social = {
+  icon: string;
+  label: string;
+  link: string;
+};
+
+export type Presentation = {
+  mail: string;
+  title: string;
+  description: string[];
+  socials: Social[];
+};
+
+export default {
+  mail: "me@dadyarri.ru",
+  title: "Салют, я Даниил 👋",
+  description: [
+    "Бэкенд-разработчик из России *с двухлетним опытом*. Сейчас я изучаю *ASP.NET* и *Jetpack Compose*",
+    "Работаю *бэкенд-разработчиком (.NET)* в ГК «Системы и технологии»",
+  ],
+  socials: [
+    {
+      icon: "mdi:github",
+      label: "Github",
+      link: "https://github.com/dadyarri",
+    },
+    {
+      icon: "mdi:telegram",
+      label: "Telegram",
+      link: "https://t.me/dadyarri",
+    },
+    {
+      icon: "mdi:email",
+      label: "Электропочта",
+      link: "mailto:me@dadyarri.ru",
+    },
+    {
+      icon: "mdi:paperclip",
+      label: "Резюме",
+      link: "/cv",
+    },
+  ],
+} as Presentation;
