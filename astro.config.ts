27fa3ec1c665a8f75f6883563d11426ab5983c0b
Daniil <dadyarri@gmail.com>
2024-01-21 13:03:49 +0300

    install code blocks modifier and some plugins to it

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig, type AstroUserConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-import mdx from "@astrojs/mdx";
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import mdx from '@astrojs/mdx';
 import { SITE_URL } from './src/data/config';
 import { remarkReadingTime } from './src/remark/reading-time';
+import { pluginCollapsibleSections } from '@expressive-code/plugin-collapsible-sections';
+
+import expressiveCode from 'astro-expressive-code';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()],
-  site: SITE_URL,
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      wrap: true
+    integrations: [
+        tailwind(),
+        icon(),
+        expressiveCode({
+            plugins: [pluginCollapsibleSections()],
+        }),
+        mdx(),
+    ],
+    site: SITE_URL,
+    markdown: {
+        syntaxHighlight: 'shiki',
+        shikiConfig: {
+            wrap: true,
+        },
+        remarkPlugins: [remarkReadingTime],
     },
-    remarkPlugins: [remarkReadingTime]
-  }
 } satisfies AstroUserConfig);
