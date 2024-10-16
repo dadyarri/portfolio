90f814080ed5ee880a0d2cb5055dbb90776f4dba
Голубев Д.О. <golubev@sicon.ru>
2024-10-16 10:28:49 +0300

    add generated help message

--- og-builder/src/structs.rs
+++ og-builder/src/structs.rs
@@ -15,7 +15,12 @@ pub struct PreambleTaxonomies {
 }
 
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 pub struct Cli {
+    #[arg(short, long)]
     pub(crate) theme: String,
+    #[arg(short, long, default_value_t = 600)]
     pub(crate) wait_for_browser_in_msec: u64,
+    #[arg(short, long, value_delimiter = ',', default_value = "posts")]
+    pub(crate) sections: Vec<String>,
 }
