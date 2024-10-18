bc4d59ac60bd32eca91f2adb673b02028141d45c
Голубев Д.О. <golubev@sicon.ru>
2024-10-18 08:46:32 +0300

    selective update from posts/uni-summary branch

--- og-builder/src/structs.rs
+++ og-builder/src/structs.rs
@@ -5,16 +5,22 @@ use serde::Deserialize;
 pub struct Preamble {
     pub(crate) title: String,
     pub(crate) date: String,
+    #[serde(default)]
     pub(crate) taxonomies: PreambleTaxonomies,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Default)]
 pub struct PreambleTaxonomies {
     pub(crate) tags: Vec<String>,
 }
 
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 pub struct Cli {
+    #[arg(short, long)]
     pub(crate) theme: String,
+    #[arg(short, long, default_value_t = 600)]
     pub(crate) wait_for_browser_in_msec: u64,
+    #[arg(short, long, value_delimiter = ',', default_value = "posts")]
+    pub(crate) sections: Vec<String>,
 }
