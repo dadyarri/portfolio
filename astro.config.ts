a0a8a2d74c240909707ba9194366bce0831c348a
Daniil <dadyarri@gmail.com>
2025-01-06 20:52:53 +0300

    get rid of svelte

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,7 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-
-import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: []
 });
