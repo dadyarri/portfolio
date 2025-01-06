6d47f256bb3f92938e44c811dea3f596845ad1cf
Daniil <dadyarri@gmail.com>
2025-01-06 22:33:19 +0300

    prepare site to build on netlify

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon()],
+  adapter: netlify()
 });
