c539130e73df0e3a93cd14f16c28332a81331a0c
Daniil <dadyarri@gmail.com>
2025-04-27 00:31:00 +0300

    add mdx integration

--- astro.config.ts
+++ astro.config.ts
@@ -7,10 +7,12 @@ import netlify from '@astrojs/netlify';
 
 import { remarkReadingTime } from './src/remark/reading-time.mjs';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dadyarri.ru",
-  integrations: [icon()],
+  integrations: [icon(), mdx()],
   adapter: netlify(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection, reference } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/data/content' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/data/content' }),
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),
