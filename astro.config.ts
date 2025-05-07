293abb3801d3eb21cf87796e76b6643dbb83192c
Daniil <dadyarri@gmail.com>
2025-05-07 20:15:00 +0300

    delete astro integration to generate og

--- astro.config.ts
+++ astro.config.ts
@@ -6,12 +6,11 @@ import icon from 'astro-icon';
 import netlify from '@astrojs/netlify';
 
 import { remarkReadingTime } from './src/remark/reading-time.mjs';
-import { ogImagesGeneratorIntegration } from './src/integrations/ogImagesGeneratorIntegration';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dadyarri.ru",
-  integrations: [icon(), ogImagesGeneratorIntegration()],
+  integrations: [icon()],
   adapter: netlify(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
