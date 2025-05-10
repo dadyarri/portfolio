4c3c100772719acf08c139d2d9bf968aeafd10fb
Daniil <dadyarri@gmail.com>
2025-05-10 18:01:24 +0300

    update og images config

---
+++ OgImages/OgImages/Configuration/Canvas.cs
@@ -0,0 +1,8 @@
+﻿namespace OgImages.Configuration;
+
+public class Canvas
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public string? Background { get; set; }
+}
--- OgImages/OgImages/Configuration/OgImagesConfiguration.cs
+++ OgImages/OgImages/Configuration/OgImagesConfiguration.cs
@@ -2,6 +2,7 @@
 
 public class OgImagesConfiguration
 {
+    public required Canvas Canvas { get; set; }
     public List<Directory> Directories { get; set; } = [];
     public List<FontConfig> Fonts { get; set; } = [];
 }
