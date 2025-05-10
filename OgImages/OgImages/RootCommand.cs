3d9b37edb7a8e7703a4b15c4ae4165e6ead033d4
Daniil <dadyarri@gmail.com>
2025-05-10 23:22:42 +0300

    add logic of parsing config and filling it with correct full paths (assuming starting from config's location)

--- OgImages/OgImages/RootCommand.cs
+++ OgImages/OgImages/RootCommand.cs
@@ -1,12 +1,14 @@
 ﻿using System.Text.Json;
+using System.Text.RegularExpressions;
 using OgImages.Configuration;
 using OgImages.Utils;
 using Spectre.Console;
 using Spectre.Console.Cli;
+using Directory = System.IO.Directory;
 
 namespace OgImages;
 
-internal sealed class RootCommand : AsyncCommand<RootCommandSettings>
+internal sealed partial class RootCommand : AsyncCommand<RootCommandSettings>
 {
     public override async Task<int> ExecuteAsync(CommandContext context, RootCommandSettings settings)
     {
@@ -24,11 +26,59 @@ internal sealed class RootCommand : AsyncCommand<RootCommandSettings>
             return 1;
         }
 
+        var configurationDirectory = Path.GetDirectoryName(configurationPath)!;
+
         AnsiConsole.MarkupLineInterpolated($"[green]Using config file from '{configurationPath}'[/]");
 
         await using var fs = File.OpenRead(configurationPath);
         var configuration = await JsonSerializer.DeserializeAsync<OgImagesConfiguration>(fs);
 
+        if (configuration is null)
+        {
+            AnsiConsole.MarkupLine("[red]Error reading configuration file[/]");
+            return 1;
+        }
+
+        foreach (var directory in configuration.Directories)
+        {
+            directory.Path = Path.GetFullPath(Path.Join(configurationDirectory, directory.Path));
+            
+            if (!Directory.Exists(directory.Path))
+            {
+                AnsiConsole.MarkupLine("[red]Directory doesn't exist[/]");
+                return 1;
+            }
+        }
+
+        foreach (var fontConfig in configuration.Fonts)
+        {
+            fontConfig.Path = Path.GetFullPath(DirectoryRegex().Replace(fontConfig.Path, match =>
+            {
+                var dirName = match.Groups[1].Value;
+                var dirPath = configuration.Directories
+                    .Where(d => d.Name == dirName)
+                    .Select(d => d.Path)
+                    .FirstOrDefault();
+
+                if (dirPath is null)
+                {
+                    AnsiConsole.MarkupLine("[red]Directory doesn't exist[/]");
+                    return string.Empty;
+                }
+                
+                return dirPath;
+            }));
+
+            if (!File.Exists(fontConfig.Path))
+            {
+                AnsiConsole.MarkupLineInterpolated($"[red]Font file {fontConfig.Name} not found[/]");
+                return 1;
+            }
+        }
+
         return 0;
     }
+
+    [GeneratedRegex("#(.*)#")]
+    private static partial Regex DirectoryRegex();
 }
