1ed7019ce07130ead538eb1cb144b701c2836814
Daniil <dadyarri@gmail.com>
2025-05-19 01:03:13 +0300

    add --all flag to generate og images for all content

--- OgImages/OgImages/RootCommand.cs
+++ OgImages/OgImages/RootCommand.cs
@@ -11,6 +11,7 @@ using SixLabors.ImageSharp.Processing;
 using Spectre.Console;
 using Spectre.Console.Cli;
 using YamlDotNet.Serialization;
+using Directory = System.IO.Directory;
 using Size = SixLabors.ImageSharp.Size;
 
 namespace OgImages;
@@ -19,7 +20,7 @@ internal sealed partial class RootCommand : AsyncCommand<RootCommandSettings>
 {
     public override async Task<int> ExecuteAsync(CommandContext context, RootCommandSettings settings)
     {
-        if (settings.Content is null)
+        if (settings.Content is null && !settings.All)
         {
             AnsiConsole.MarkupLine("[red]Please select content item[/]");
             return 1;
@@ -58,8 +59,6 @@ internal sealed partial class RootCommand : AsyncCommand<RootCommandSettings>
             directories.TryAdd(directory.Name, Path.GetFullPath(Path.Join(configurationDirectory, directory.Path)));
         }
 
-        directories.TryAdd("item", settings.Content);
-
         var fontCollection = new FontCollection();
         Dictionary<string, Tuple<FontFamily, FontStyle>> fonts = [];
 
@@ -84,51 +83,73 @@ internal sealed partial class RootCommand : AsyncCommand<RootCommandSettings>
             fonts.Add(fontConfig.Name, new Tuple<FontFamily, FontStyle>(family, style));
         }
 
-        var postContent =
-            await File.ReadAllTextAsync(FileUtils.GetFullPath(configuration.Content.Article, directories));
-        var frontmatterString = FrontmatterRegex().Match(postContent).Groups[1].Value;
-        var frontmatter = new Deserializer().Deserialize<Dictionary<string, object?>>(frontmatterString);
+        List<string> contentItems = [];
 
-        foreach (var kvp in frontmatter)
+        if (settings.All)
         {
-            frontmatter[kvp.Key] = TypeUtils.GuessType(kvp.Value);
-        }
+            var pathParts = configuration.Content.Article.Split('/');
+            var contentRoot = FileUtils.GetFullPath(pathParts.First(), directories);
+            var fileName = pathParts.Last();
 
-        using var og = new Image<Rgba32>(new SixLabors.ImageSharp.Configuration(), configuration.Canvas.Width,
-            configuration.Canvas.Height);
-
-        var backgroundPath = FileUtils.GetFullPath(configuration.Canvas.Background.Image, directories);
-        if (File.Exists(backgroundPath))
-        {
-            using var backgroundImage = Image.Load<Rgba32>(backgroundPath);
-            backgroundImage.Mutate(ctx =>
-                ctx.Resize(new Size(configuration.Canvas.Width, configuration.Canvas.Height)));
-
-            og.Mutate(ctx =>
-            {
-                var rect = new Rectangle(0, 0, configuration.Canvas.Width, configuration.Canvas.Height);
-                ctx.DrawImage(backgroundImage, new Point(0, 0), 1f);
-                ctx.Fill(configuration.Canvas.Background.OverlayColor, rect);
-            });
+            contentItems = Directory.EnumerateFiles(contentRoot, fileName, SearchOption.AllDirectories)
+                .Select(path => Path.GetRelativePath(contentRoot, Path.GetDirectoryName(path)!))
+                .ToList();
         }
         else
         {
-            og.Mutate(ctx => ctx.Fill(configuration.Canvas.Background.FallbackColor));
+            contentItems = [settings.Content];
         }
 
-
-        og.Mutate(ctx =>
+        foreach (var contentItem in contentItems)
         {
-            var lm = new LayoutManager(configuration.Layers);
-            lm.Render(ctx, configuration, frontmatter, fonts, directories);
-        });
+            directories["item"] = contentItem;
 
-        var ogImagePath = FileUtils.GetFullPath(configuration.Content.OgImage, directories);
-        using var ms = new MemoryStream();
-        await using var outputFileStream = new FileStream(ogImagePath, FileMode.OpenOrCreate);
+            var postContent =
+                await File.ReadAllTextAsync(FileUtils.GetFullPath(configuration.Content.Article, directories));
+            var frontmatterString = FrontmatterRegex().Match(postContent).Groups[1].Value;
+            var frontmatter = new Deserializer().Deserialize<Dictionary<string, object?>>(frontmatterString);
 
-        await og.SaveAsync(ms, new PngEncoder());
-        ms.WriteTo(outputFileStream);
+            foreach (var kvp in frontmatter)
+            {
+                frontmatter[kvp.Key] = TypeUtils.GuessType(kvp.Value);
+            }
+
+            using var og = new Image<Rgba32>(new SixLabors.ImageSharp.Configuration(), configuration.Canvas.Width,
+                configuration.Canvas.Height);
+
+            var backgroundPath = FileUtils.GetFullPath(configuration.Canvas.Background.Image, directories);
+            if (File.Exists(backgroundPath))
+            {
+                using var backgroundImage = Image.Load<Rgba32>(backgroundPath);
+                backgroundImage.Mutate(ctx =>
+                    ctx.Resize(new Size(configuration.Canvas.Width, configuration.Canvas.Height)));
+
+                og.Mutate(ctx =>
+                {
+                    var rect = new Rectangle(0, 0, configuration.Canvas.Width, configuration.Canvas.Height);
+                    ctx.DrawImage(backgroundImage, new Point(0, 0), 1f);
+                    ctx.Fill(configuration.Canvas.Background.OverlayColor, rect);
+                });
+            }
+            else
+            {
+                og.Mutate(ctx => ctx.Fill(configuration.Canvas.Background.FallbackColor));
+            }
+
+
+            og.Mutate(ctx =>
+            {
+                var lm = new LayoutManager(configuration.Layers);
+                lm.Render(ctx, configuration, frontmatter, fonts, directories);
+            });
+
+            var ogImagePath = FileUtils.GetFullPath(configuration.Content.OgImage, directories);
+            using var ms = new MemoryStream();
+            await using var outputFileStream = new FileStream(ogImagePath, FileMode.OpenOrCreate);
+
+            await og.SaveAsync(ms, new PngEncoder());
+            ms.WriteTo(outputFileStream);
+        }
 
         return 0;
     }
--- OgImages/OgImages/RootCommandSettings.cs
+++ OgImages/OgImages/RootCommandSettings.cs
@@ -5,6 +5,10 @@ namespace OgImages;
 
 public class RootCommandSettings : CommandSettings
 {
+    [Description("Flag to generate all open graph images")]
+    [CommandOption("--all")]
+    public bool All { get; set; }
+    
     [Description("Content ID to generate images")]
     [CommandArgument(0, "[content]")]
     public string? Content { get; set; }
