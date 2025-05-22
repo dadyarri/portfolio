3a23118d35a096ec1ab2ceb3c1aa46b0965f86c4
Daniil <dadyarri@gmail.com>
2025-05-22 08:25:01 +0300

    include minis to rss feed

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,23 +1,44 @@
 import rss from '@astrojs/rss';
-import { getPosts } from '@utils/content';
+import { getMinis, getPosts } from '@utils/content';
 import { splitArrayByElement, stripHtml } from '@utils/data';
 import type { APIContext } from 'astro';
 
 export async function GET(context: APIContext) {
+
+  const posts = (await getPosts()).map(post => ({
+    title: post.data.title,
+    pubDate: post.data.date,
+    description: stripHtml(splitArrayByElement(
+      post.rendered?.html.split("\n")!,
+      "<!--more-->",
+    ).preview.join("")),
+    link: `/posts/${post.id}`,
+    author: "dadyarri"
+  }));
+  
+  const minis = (await getMinis()).map(mini => ({
+    title: mini.data.title,
+    pubDate: mini.data.date,
+    description: stripHtml(splitArrayByElement(
+      mini.rendered?.html.split("\n")!,
+      "<!--more-->",
+    ).preview.join("")),
+    link: `/minis/${mini.id}`,
+    author: "dadyarri"
+  }))
+
+  const contentItems = posts.concat(minis).sort((a, b) => {
+    const dateA = a.pubDate.valueOf();
+    const dateB = b.pubDate.valueOf();
+
+    return dateB - dateA;
+  });
+
   return rss({
     title: 'Личный блог dadyarri',
     description: 'Опыт разработки, наблюдения и хобби',
     site: context.site!,
-    items: (await getPosts()).map((post) => ({
-        title: post.data.title,
-        pubDate: post.data.date,
-        description: stripHtml(splitArrayByElement(
-            post.rendered?.html.split("\n")!,
-            "<!--more-->",
-        ).preview.join("")),
-        link: `/posts/${post.id}`,
-        author: "dadyarri"
-    })),
+    items: contentItems,
     customData: `<language>ru-ru</language>`,
   });
 }
