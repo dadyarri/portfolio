a20a5bf61a2646280ba5876f724dafc656e83ae8
Daniil <dadyarri@gmail.com>
2025-08-06 10:24:55 +0300

    remove weird dependency

--- astro.config.ts
+++ astro.config.ts
@@ -3,15 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
-import netlify from '@astrojs/netlify';
-
 import { remarkReadingTime } from './src/remark/reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://dadyarri.ru",
   integrations: [icon()],
-  adapter: netlify(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
